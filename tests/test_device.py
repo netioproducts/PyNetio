015e99bfa333c5cdb4a3916754d0cd9481959b76
Adam Verner <averner@netio.eu>
2023-09-21 13:20:33 +0200

    add netio demos into testing environment

--- tests/test_device.py
+++ tests/test_device.py
@@ -1,12 +1,38 @@
 import pytest
 
-from Netio.Device import Device
+from Netio import Netio
+
+ONLINE_DEMOS = [
+    ("http://powerpdu-8qs.netio-products.com/netio.json", ("netio", "netio")),
+    ("http://netio-4c.netio-products.com:8080/netio.json", ("", "")),
+    #    ("http://powerdin-4pz.netio-products.com:22888/netio.json", ("netio", "netio")),  # json not enabled
+    ("http://powercable-2kz.netio-products.com/netio.json", ("netio", "netio")),
+    ("http://powercable-2pz.netio-products.com/netio.json", ("netio", "netio")),
+    ("http://pc-rest.netio-products.com:22888/netio.json", ("netio", "netio")),
+    ("http://powerpdu-4ps.netio-products.com:22888/netio.json", ("netio", "netio")),
+    ("http://powerbox-3px.netio-products.com:22888/netio.json", ("netio", "netio")),
+    ("http://powerbox-4kx.netio-products.com/netio.json", ("netio", "netio")),
+    # ("http://pc-mqtt.netio-products.com:22888/netio.json", ("netio", "netio")),  # Obsolete
+    # ("http://pc-modbus.netio-products.com:22888/netio.json", ("netio", "netio")),  # Obsolete
+    # ("http://netio-4all.netio-products.com/netio.json", ("netio", "netio")),  # Obsolete
+    # ("http://netio-4.netio-products.com/netio.json", ("netio", "netio")),  # Obsolete
+]
+
+
+def ids(param):
+    return param[0].split(".")[0].split("/")[-1]
 
 
 @pytest.fixture
-def device():
-    return Device("https://")
+def netio_device(request):
+    url, auth = request.param
+    return Netio(url, auth_rw=auth, timeout=5)
 
 
-def test_get_output(device, requests_mock):
-    raise NotImplementedError()
+@pytest.mark.parametrize("netio_device", ONLINE_DEMOS, indirect=True, ids=ids)
+def test_get_output(netio_device: Netio):
+    print(netio_device)
+    netio_device.get_outputs()
+    netio_device.set_output(1, 0)
+    netio_device.set_output(1, netio_device.ACTION.ON)
+    netio_device.get_output(1)
